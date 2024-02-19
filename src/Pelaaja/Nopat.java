a6a6bfbd9662bf0bda12f4b90073abc600685627
Saimou0 <91085350+Saimou0@users.noreply.github.com>
2024-02-19 09:57:19 +0200

    Update Nopat.java
    Poistin turhan koodin

--- src/Pelaaja/Nopat.java
+++ src/Pelaaja/Nopat.java
@@ -42,46 +42,4 @@ public class Nopat {
     public boolean[] getLukitutNopat() {
         return lukitutNopat;
     }
-
-        while (heittojaJaljella > 0) {
-            kayttoliittyma.piirraNopat(nopat);
-
-            System.out.println("Heittoja jäljellä: " + heittojaJaljella);
-            System.out.println("Valitse toiminto:");
-            System.out.println("1 -> Heitä nopat");
-            System.out.println("2 -> Lukitse noppia");
-            System.out.println("3 -> Valitse pisteet");
-            System.out.println("0 -> Lopeta");
-
-            int valinta = scanner.nextInt();
-
-            switch (valinta) {
-                case 1:
-                    heitaNopat();
-                    heittojaJaljella--;
-                    break;
-
-                case 2:
-                    kayttoliittyma.piirraNopat(nopat);
-                    System.out.println("Anna lukittavien noppien indeksit (esim. 1 3 5):");
-                    String lukittavatNopat = scanner.nextLine();
-                    String[] indeksit = lukittavatNopat.split(" ");
-                    for (String indeksi : indeksit) {
-                        lukitseNoppa(Integer.parseInt(indeksi) - 1);
-                    }
-                    break;
-
-                case 3:
-                    break;
-
-                case 0:
-                    heittojaJaljella = 0;
-                    break;
-
-                default:
-                    kayttoliittyma.piirraVirheSyotto();
-                    break;
-            }
-        }
-    }
 }
